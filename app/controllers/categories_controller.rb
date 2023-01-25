98863fd37a75232a031860ae2afb808dd0167e02
anironL <viasolaris@gmail.com>
2023-01-25 16:27:56 -0500

    Replace placeholder authentication with .env variables

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book", except: [:index, :show]
-
+  
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
