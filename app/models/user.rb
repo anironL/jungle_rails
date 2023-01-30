4ed2736a2fc2befc5e5be3e1a78396c2f8774a18
anironL <viasolaris@gmail.com>
2023-01-30 15:48:15 -0500

    Add validation testing for f/l name, email, & password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -4,30 +4,79 @@ RSpec.describe UserSpec, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe 'Validations' do
     before :each do
-      @test_user = User.new
-        @test_user.name = "test1_name" 
-        @test_user.email = "test1@test.com" 
-        @test_user.password = "test1_password" 
-        @test_user.password_confirmation = "test1_password" 
+      @test_user1 = User.new
+        @test_user1.first_name = "test1_Fname" 
+        @test_user1.last_name = "test1_Lname" 
+        @test_user1.email = "test1@test.com" 
+        @test_user1.password = "test1_password" 
+        @test_user1.password_confirmation = "test1_password" 
 
-        @test_user.save
-    end
+        @test_user1.save
+      end
 
   # It must be created with a password and password_confirmation fields
-  it "Check that it fails if password field is missing" do
-    @test_user2 = User.new(name: "test2_name", email: "test2@test.com", password: "", password_confirmation: "test2_password")
-    @test_user2.save
+    it "Check that it fails if password field is missing" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "test2@test.com", password: "", password_confirmation: "test2_password")
+      @test_user2.save
 
-    expect(@test_user2.errors.full_messages).to include("Password can't be blank")
-  end
+      expect(@test_user2.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "Check that it fails if password confirmation field is missing" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "test2@test.com", password: "test2_password", password_confirmation: "")
+      @test_user2.save
+
+      expect(@test_user2.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
   
   # These need to match so you should have an example for where they are not the same
-  
+    it "Check that it fails if password does not match password confirmation" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "test2@test.com", password: "test2_password", password_confirmation: "test2_wrongpassword")
+      @test_user2.save
+
+      expect(@test_user2.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
   # These are required when creating the model so you should also have an example for this
+    # Email, first name, and last name should also be required
+    it "Check that it fails if there is no first name" do
+      @test_user2 = User.new(first_name: "", last_name: "test2_Lname", email: "test2@test.com", password: "test2_password", password_confirmation: "test2_password")
+      @test_user2.save
+  
+      expect(@test_user2.errors.full_messages).to include("First name can't be blank")
+    end
+  
+    it "Check that it fails if there is no last name" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "", email: "test2@test.com", password: "test2_password", password_confirmation: "test2_password")
+      @test_user2.save
+  
+      expect(@test_user2.errors.full_messages).to include("Last name can't be blank")
+    end
+  
+    it "Check that it fails if there is no last email" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "", password: "test2_password", password_confirmation: "test2_password")
+      @test_user2.save
+      
+      expect(@test_user2.errors.full_messages).to include("Email can't be blank")
+    end
+    
+    # Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
+    it "Check that it fails if email is taken and for case insensitivity" do      
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "teSt1@tEst.coM", password: "test2_password", password_confirmation: "test2_password")
+      @test_user2.save
 
-  # Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
 
-  # Email, first name, and last name should also be required
+      expect(@test_user2.errors.full_messages).to include("Email has already been taken")
+    end
 
-end
+  # The password must have a minimum length when a user account is being created.
+    it "Check that it fails if password is < 5 characters" do
+      @test_user2 = User.new(first_name: "test2_Fname", last_name: "test2_Lname", email: "test2@test.com", password: "shor", password_confirmation: "shor")
+      @test_user2.save
+
+      expect(@test_user2.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+
+  
+  end
 end
