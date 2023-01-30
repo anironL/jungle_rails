671dd9744465c48c83837e7ea53767e17eaf30ca
anironL <viasolaris@gmail.com>
2023-01-30 16:47:21 -0500

    Move login logic to User class method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  attr_accessor :email, :password
+
+  def self.authenticate_with_credentials (email, password)
+    user = User.find_by_email(email)
+    # If the user exists and the password entered is correct.
+    if user && user.authenticate(password)
+      return user
+    end
+
+    return false
+  end
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
